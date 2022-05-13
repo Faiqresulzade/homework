e7c45bff13b0a9e2e8471f833fae2dd8de87e8f1
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-13 18:38:58 +0400

    added task2

---
+++ 13maytask2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_may_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 60, 120, 140, 19 };
+            int avg = CalcAvg(arr);
+
+
+            if (avg > 60)
+            {
+                Console.WriteLine("Mezun oldunuz");
+            }
+            else
+            {
+                Console.WriteLine("Mezun ola bilmediniz");
+            }
+
+        }
+        static int CalcAvg(int[] arr)
+        {
+            int sum = 0;
+            int say = 0;
+            int avg = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+                say++;
+            }
+            avg = sum / say;
+
+            return avg;
+        }
+    }
+}
