efe1f169b05c7c9eb9175479c3f2448407cf8f38
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-18 13:00:37 +0400

    added 18may task

---
+++ 18maytask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _18maytask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,3,5,6};
+            int[] arr2 = { 3, 6 };
+            string joinarr = string.Join(" ", arr);
+            string joinarr2 = string.Join(" ", arr2);
+
+            Console.WriteLine(joinarr.Contains(joinarr2));
+
+           
+        }
+    }
+}
