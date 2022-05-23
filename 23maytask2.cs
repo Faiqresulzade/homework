6b1a85b76bcf947471949cc489a8bccce30e1a2e
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-23 13:37:49 +0400

    added

---
+++ 23maytask2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string [] name={"faiq","beyler","mirqubad"};
+            char herf = 'i';
+            namechecked(name, herf);
+        }
+
+        static void namechecked(string[] name ,char herf)
+        {
+            foreach (var item in name)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] == herf)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+            }
+           
+        }
+    }
+}
