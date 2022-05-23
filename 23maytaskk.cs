81eec55d412fe6e8897402b64f6c0b2ddec17c53
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-23 13:38:12 +0400

    added

---
+++ 23maytaskk.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 2, 4, 6, 8, 1, 3, 5, 9 };
+            SortArray(ref num);
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+        }
+
+        static Array SortArray(ref int[] num)
+        {
+
+            int temp = 0;
+            for (int i = 0; i < num.Length - 1; i++)
+            {
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    if (num[i] > num[j])
+                    {
+                        temp = num[i];
+                        num[i] = num[j];
+                        num[j] = temp;
+
+                    }
+                }
+            }
+            return num;
+        }
+    }
+}
