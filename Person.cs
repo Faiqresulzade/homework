55d65f1642ee3edf7a07518c4ca76fc86501a2d1
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-05-24 18:07:03 +0400

    added

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _24maytask
+{
+    class Person
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public bool IsAdult;
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            IsAdult = false;
+            if (age > 18)
+            {
+                IsAdult = true;
+                Console.WriteLine(IsAdult);
+            }
+
+            else
+                Console.WriteLine(IsAdult);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _24maytask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person iamperson = new Person("Faiq", "Resulzade", 20);
+
+        }
+    }
+}
